98e4803de561bdeadaac94857c6cee54a51d52a2
Awofala Gbolahan <edawofal@gmail.com>
2024-11-30 20:07:07 +0100

    Fixed https

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Search = async (query) => {
     const api_key = '203c93015a9d4c4eb3a135800242811';
-    const url = `http://api.weatherapi.com/v1/search.json?key=${api_key}&q=${query}`
+    const url = `https://api.weatherapi.com/v1/search.json?key=${api_key}&q=${query}`
 
     try {
         const response = await axios.get(url)
